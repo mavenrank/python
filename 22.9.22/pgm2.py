f4130ffb133654868b981b10f8354dee7a33feb1
git4ananth <114994781+git4ananth@users.noreply.github.com>
2023-07-09 08:06:35 +0530

    Area of Rectangle

---
+++ 22.9.22/pgm2.py
@@ -0,0 +1,5 @@
+# Find the area of a rectangle
+length = float(input("Enter the length of the rectangle : "))
+breadth = float(input("Enter the breadth of the reactangle : "))
+area = length * breadth
+print("Area of the rectangle is : ", area, " units")
