e2328eb442a2d1b56b102054db6ca5303ad730a1
git4ananth <114994781+git4ananth@users.noreply.github.com>
2023-07-09 08:06:55 +0530

    Check if positive or negative or 0

---
+++ 22.9.22/pgm3&4.py
@@ -0,0 +1,8 @@
+# Find if the given number is positive or negative
+num = float(input("Enter a number : "))
+if num > 0:
+    print("It's a Positive Number")
+elif num < 0:
+    print("It's a Negative Number")
+else:
+    print("Its Neutral")
